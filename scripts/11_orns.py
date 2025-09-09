7dbab239bf52e6fc707fd08b4a01f50f3a80a217
Krzysztof Basinski <k.basinski@gumed.edu.pl>
2025-09-09 16:37:10 +0200

    bug in peak measures

--- scripts/11_orns.py
+++ scripts/11_orns.py
@@ -1,12 +1,13 @@
 # %%
+import pathlib
 import mne
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-
+import shared_functions as sf
 # %%
 # filepath to evokeds pickle
-evokeds_fpath = '../results/evokeds/all.p'
+evokeds_fpath = "../results/evokeds/all.p"
 
 # participants list
 participants = list(range(1, 27)) + list(range(29, 38))
@@ -14,69 +15,138 @@ participants = list(range(1, 27)) + list(range(29, 38))
 # channel picks for plotting
 channel_picks = ["F3", "Fz", "F4", "FC1", "FC2"]
 
+# raw data filepath for channel adjacency
+raw_path = pathlib.Path("/Users/kbas/sci_data/harmonicity_data/raw")
+
+# filepath for plots
+figpath = "../results/plots/"
+
 # unpickle the evokeds
-with open(evokeds_fpath, 'rb') as f:
+with open(evokeds_fpath, "rb") as f:
     evokeds = pickle.load(f)
 # %%
 # ORN for inharmonic
-evokeds['orn'] = {
-    'inharmonic_standard': {},
-    'changing_standard': {},
-    'inharmonic_deviant': {},
-    'changing_deviant': {},
-    }
+evokeds["orn"] = {
+    "inharmonic_standard": {},
+    "changing_standard": {},
+    "inharmonic_deviant": {},
+    "changing_deviant": {},
+}
 for p in participants:
-    h = evokeds['harmonic']['standard'][p]
-    i = evokeds['inharmonic']['standard'][p]
-    c = evokeds['inharmonic_changing']['standard'][p]
-    evokeds['orn']['inharmonic_standard'][p] = mne.combine_evoked([i, h], weights=[1, -1])
-    evokeds['orn']['changing_standard'][p] = mne.combine_evoked([c, h], weights=[1, -1])
-    h = evokeds['harmonic']['deviant_1'][p]
-    i = evokeds['inharmonic']['deviant_1'][p]
-    c = evokeds['inharmonic_changing']['deviant_1'][p]
-    evokeds['orn']['inharmonic_deviant'][p] = mne.combine_evoked([i, h], weights=[1, -1])
-    evokeds['orn']['changing_deviant'][p] = mne.combine_evoked([c, h], weights=[1, -1])
+    h = evokeds["harmonic"]["standard"][p]
+    i = evokeds["inharmonic"]["standard"][p]
+    c = evokeds["inharmonic_changing"]["standard"][p]
+    evokeds["orn"]["inharmonic_standard"][p] = mne.combine_evoked(
+        [i, h], weights=[1, -1]
+    )
+    evokeds["orn"]["changing_standard"][p] = mne.combine_evoked([c, h], weights=[1, -1])
+    h = evokeds["harmonic"]["deviant_1"][p]
+    i = evokeds["inharmonic"]["deviant_1"][p]
+    c = evokeds["inharmonic_changing"]["deviant_1"][p]
+    evokeds["orn"]["inharmonic_deviant"][p] = mne.combine_evoked(
+        [i, h], weights=[1, -1]
+    )
+    evokeds["orn"]["changing_deviant"][p] = mne.combine_evoked([c, h], weights=[1, -1])
 # %%
 comp = {
-    'harmonic standard': list(evokeds['harmonic']['standard'].values()),
-    'inharmonic standard': list(evokeds['inharmonic']['standard'].values()),
-    'difference (ORN)': list(evokeds['orn']['inharmonic_standard'].values())
+    "harmonic standard": list(evokeds["harmonic"]["standard"].values()),
+    "inharmonic standard": list(evokeds["inharmonic"]["standard"].values()),
+    "difference (ORN)": list(evokeds["orn"]["inharmonic_standard"].values()),
 }
 
-mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine='mean')
+mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine="mean")
 # %%
 comp = {
-    'harmonic standard': list(evokeds['harmonic']['standard'].values()),
-    'changing standard': list(evokeds['inharmonic_changing']['standard'].values()),
-    'difference (ORN)': list(evokeds['orn']['changing_standard'].values())
+    "harmonic standard": list(evokeds["harmonic"]["standard"].values()),
+    "changing standard": list(evokeds["inharmonic_changing"]["standard"].values()),
+    "difference (ORN)": list(evokeds["orn"]["changing_standard"].values()),
 }
 
-mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine='mean')
+mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine="mean")
 # %%
 comp = {
-    'harmonic deviant': list(evokeds['harmonic']['deviant_1'].values()),
-    'inharmonic deviant': list(evokeds['inharmonic']['deviant_1'].values()),
-    'difference (ORN)': list(evokeds['orn']['inharmonic_deviant'].values())
+    "harmonic deviant": list(evokeds["harmonic"]["deviant_1"].values()),
+    "inharmonic deviant": list(evokeds["inharmonic"]["deviant_1"].values()),
+    "difference (ORN)": list(evokeds["orn"]["inharmonic_deviant"].values()),
 }
 
-mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine='mean')
+mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine="mean")
 # %%
 comp = {
-    'harmonic deviant': list(evokeds['harmonic']['deviant_1'].values()),
-    'changing deviant': list(evokeds['inharmonic_changing']['deviant_1'].values()),
-    'difference (ORN)': list(evokeds['orn']['changing_deviant'].values())
+    "harmonic deviant": list(evokeds["harmonic"]["deviant_1"].values()),
+    "changing deviant": list(evokeds["inharmonic_changing"]["deviant_1"].values()),
+    "difference (ORN)": list(evokeds["orn"]["changing_deviant"].values()),
 }
 
-mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine='mean')
+mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine="mean")
 # %%
 comp = {
-    'standard harmonic vs inharmonic': list(evokeds['orn']['inharmonic_standard'].values()),
-    'standard harmonic vs changing': list(evokeds['orn']['changing_standard'].values()),
-    'deviant harmonic vs inharmonic': list(evokeds['orn']['inharmonic_deviant'].values()),
-    'deviant harmonic vs changing': list(evokeds['orn']['changing_deviant'].values()),
+    "standard harmonic vs inharmonic": list(
+        evokeds["orn"]["inharmonic_standard"].values()
+    ),
+    "standard harmonic vs changing": list(evokeds["orn"]["changing_standard"].values()),
+    "deviant harmonic vs inharmonic": list(
+        evokeds["orn"]["inharmonic_deviant"].values()
+    ),
+    "deviant harmonic vs changing": list(evokeds["orn"]["changing_deviant"].values()),
 }
 
-mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine='mean')
-# direct comparison of ORN waves
+mne.viz.plot_compare_evokeds(comp, picks=channel_picks, combine="mean")
 
+# %%
+# compare ORN topography against MMN
+mmn_ga = mne.grand_average(list(evokeds["harmonic"]["mismatch_1"].values()))
+orn_ga = mne.grand_average(list(evokeds["orn"]["inharmonic_standard"].values()))
+# %%
+topo_times = np.arange(0.1, 0.22, 0.02)
+vlims = (-1.8, 1.8)
+
+fig = mmn_ga.plot_topomap(times=topo_times, vlim=vlims, show=False)
+fig.suptitle('MMN topographies')
+sf.save_plot('sf3a.png')
+
+fig = orn_ga.plot_topomap(times=topo_times, vlim=vlims, show=False);
+fig.suptitle('ORN topographies')
+sf.save_plot('sf3b.png')
+
+# Combine
+c = [
+    "sf3a.png",
+    "sf3b.png",
+]
+c = [f"{figpath}{i}" for i in c]
+sf.stack_images(c, f"{figpath}sf3.png", padding="right")
+# %%
+def cluster_compare_mmn_orn(mmn_source, orn_source):
+    # cluster_based permutations - MMN vs ORN
+    t_window = (.1, .2)
+    # prepare data
+    a = np.array([i.crop(*t_window).get_data() for i in evokeds[mmn_source]['mismatch_1'].values()])
+    b = np.array([i.crop(*t_window).get_data() for i in evokeds['orn'][orn_source].values()])
+
+    # transpose so it fits the shape
+    a_t = np.transpose(a, (0, 2, 1))
+    b_t = np.transpose(b, (0, 2, 1))
+
+    # cluster permutations
+    cluster_stats = mne.stats.spatio_temporal_cluster_1samp_test(
+        a_t - b_t,
+        n_permutations=1024*8,
+        tail=0,
+        threshold=None,
+        n_jobs=-1,
+        adjacency=adjacency,
+        out_type="mask",
+    )
+    return cluster_stats
+
+mmn_sources = ['harmonic', 'inharmonic']
+orn_sources = ['inharmonic_standard', 'inharmonic_deviant', 'changing_standard']
+
+i = 1
+for ms in mmn_sources:
+    for os in orn_sources:
+        res  = cluster_compare_mmn_orn(ms, os)
+        print(f'{i}) Comparing {ms} - {os}: {np.where(res[2] < .05)}')
+        i += 1
 # %%
--- scripts/12_behavioral_analysis.py
+++ scripts/12_behavioral_analysis.py
@@ -5,81 +5,108 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import shared_functions as sf
 
-data_fpath = '../paradigms/behavioral/data/'
+data_fpath = "../paradigms/behavioral/data/"
 figpath = "../results/plots/"
-plist = list(range(1,16))
+plist = list(range(1, 16))
 
 
 # %%
 # load data
 dfs = []
 for pid in plist:
-    df = pd.read_csv(f'{data_fpath}{pid}_demography.csv')
+    df = pd.read_csv(f"{data_fpath}{pid}_demography.csv")
     dfs.append(df)
 
 demo = pd.concat(dfs)
 
 dfs = []
 for pid in plist:
-    df = pd.read_csv(f'{data_fpath}responses_{pid}.csv')
+    df = pd.read_csv(f"{data_fpath}responses_{pid}.csv")
     dfs.append(df)
 
 data = pd.concat(dfs)
 
 # do some cleaning
-data.response = data.response.replace({
-    'nie wiem': pd.NA
-})
+data.response = data.response.replace({"nie wiem": pd.NA})
 data.response = pd.to_numeric(data.response)
 
-data.condition = data.condition.replace({
-    'h': 'harmonic',
-    'i': 'inharmonic',
-    'ic': 'changing'
-})
+data.condition = data.condition.replace(
+    {"h": "harmonic", "i": "inharmonic", "ic": "changing"}
+)
 
-dt1 = data[data.task == 't1'].copy()
-dt2 = data[data.task == 't2'].copy()
+dt1 = data[data.task == "t1"].copy()
+dt2 = data[data.task == "t2"].copy()
 
-dt1.response = dt1.response.replace({
-    1: 'One sound',
-    2: 'Two sounds',
-    3: "Three or more sounds"
-})
+dt1.response = dt1.response.replace(
+    {1: "One sound", 2: "Two sounds", 3: "Three or more sounds"}
+)
 
 # calculate difference between response and ground truth
-dt2['error'] = dt2['response'] - dt2['gt']
-dt2['abs_error'] = dt2.error.abs()
+dt2["error"] = dt2["response"] - dt2["gt"]
+dt2["abs_error"] = dt2.error.abs()
+
+# apply additional jitter for plotting
+dt2['abs_error_jit'] = dt2['abs_error'] + np.random.uniform(-.3, .3, len(dt2))
+
+# fix indexing
+dt2r = dt2.reset_index()
 
 # drop to csv
-dt1.to_csv(f'{data_fpath}behavioral_task1.csv')
-dt2.to_csv(f'{data_fpath}behavioral_task2.csv')
+dt1.to_csv(f"{data_fpath}behavioral_task1.csv")
+dt2.to_csv(f"{data_fpath}behavioral_task2.csv")
+
+
 # %%
 # summarize demography
-print(f'Median age was {demo.Wiek.median().round(2)}, SD = {demo.Wiek.std().round(2)}.')
-print('Gender percentages:')
+print(f"Median age was {demo.Wiek.median().round(2)}, SD = {demo.Wiek.std().round(2)}.")
+print("Gender percentages:")
 print(demo.Plec.value_counts())
 # %%
-
+# summarize ground truth stats
+dt2r['gt'].mean().round(2)
+# %%
+dt2r['gt'].std().round(2)
+# %%
+dt2r['gt'].max().round(2)
 # %%
 
 # look at errors between conditions
 # %%
-condition_list = ['harmonic', 'inharmonic', 'changing']
+condition_list = ["harmonic", "inharmonic", "changing"]
 colors = sf.default_colors(condition_list)
 
 fig, axs = plt.subplots(1, 2, figsize=(9, 3.5))
 
+
 # Task 1 results
-sns.countplot(dt1, x='condition', hue='response', ax=axs[0], order=condition_list)
+sns.countplot(dt1, x="condition", hue="response", ax=axs[0], order=condition_list)
 
 # Task 2 results
-sns.boxplot(data=dt2, x='condition', y='abs_error', palette=colors, order=condition_list, ax=axs[1])
+sns.boxplot(
+    data=dt2r,
+    x="condition",
+    y="abs_error",
+    palette=colors,
+    order=condition_list,
+    ax=axs[1],
+    fliersize=0,
+)
+sns.stripplot(
+    data=dt2r,
+    x="condition",
+    y="abs_error_jit",
+    order=condition_list,
+    size=2,
+    color="black",
+    jitter=.2,
+    ax=axs[1],
+)
 
-axs[0].set_ylabel('no. responses')
-axs[0].set_title('A) Task 1 results')
-axs[1].set_ylabel('|error|')
-
-plt.savefig(f'{figpath}behavioral_tasks.png', dpi=300)
+axs[0].set_ylabel("no. responses")
+axs[0].set_title("A)", loc="left")
+axs[1].set_ylabel("|error|")
+axs[1].set_title("B), loc="left"")
+plt.tight_layout()
+plt.savefig(f"{figpath}behavioral_tasks.png", dpi=300)
 
 # %%
